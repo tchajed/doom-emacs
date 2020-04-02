d1d55117ec4b42bb9dc152fa81bb07e042d28d92
Tej Chajed <tchajed@mit.edu>
2020-04-02 09:27:18 -0500

    lang/coq: bump proof-general to latest commit

--- modules/lang/coq/packages.el
+++ modules/lang/coq/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/coq/packages.el
 
-(package! proof-general :pin "9196749d55")
+(package! proof-general :pin "71748af5bc")
 (package! company-coq :pin "f9dba9ddff")
